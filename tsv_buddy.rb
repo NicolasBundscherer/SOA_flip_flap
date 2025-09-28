7b5428af7fff01929730179c93aacc97b55cfdc9
nicolas.bundscherer <nicolas.bundscherer@polytechnique.edu>
2025-09-28 15:03:36 +0800

    All tests pass; rubocop/flog resolved

--- tsv_buddy.rb
+++ tsv_buddy.rb
@@ -6,13 +6,32 @@ module TsvBuddy
   # arguments: tsv - a String in TSV format
   # returns: nothing
   def take_tsv(tsv)
+    lines = tsv.split("\n")
+    @in_categories = lines.shift.split("\t")
+    @data = lines.map { |line| line_to_hash(line) }
+  end
 
+  def line_to_hash(line)
+    entries = line.chomp.split("\t")
+    @in_categories.zip(entries).to_h
   end
 
   # Converts @data into tsv string
   # arguments: none
   # returns: String in TSV format
   def to_tsv
+    @out_categories = @data[0].keys
+    output = parse_header
+    output << parse_body.join("\n")
+    output << "\n"
+    output
+  end
 
+  def parse_header
+    "#{@out_categories.join("\t")}\n"
+  end
+
+  def parse_body
+    @data.map { |line| line.values.join("\t") }
   end
 end
